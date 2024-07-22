d34dcb8a4ae97dc540c3e784265c704164cabbe8
Chen Yu Ling <yo0.guitar.it@gmail.com>
2024-07-23 01:16:44 +0800

    refactor : add PlayerContextType

--- client/src/hooks/usePlayer.ts
+++ client/src/hooks/usePlayer.ts
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContextType } from "../types/types";
 
-export const usePlayer = () => {
+export const usePlayer = (): PlayerContextType => {
   const context = useContext(PlayerContext);
+
   if (context === undefined) {
     throw new Error("usePlayer must be used within a PlayerProvider");
   }
+
   return context;
 };
